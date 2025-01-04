d55b602b8559d9fcfda03a1c30fe4d8d8d821f4d
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2025-01-04 16:39:46 +0100

    feat: lower quality values as much as possible, disable screen shake

--- scripts/tanktrouble-booster.js
+++ scripts/tanktrouble-booster.js
@@ -1,72 +1,69 @@
 // ==UserScript==
-// @name Tank Trouble Booster
+// @name TankTrouble Booster
 // @namespace http://tampermonkey.net/
-// @version 0.0.1
+// @version      2025.1.4
 // @license      GPL-3.0
-// @description Boost Chrome performance for Tank Trouble game by disabling certain features
+// @description Boost performance for TankTrouble game by lowering in game quality and its values.
 // @author kamarov
 // @match https://tanktrouble.com/*
 // @grant none
-// @downloadURL https://update.greasyfork.org/scripts/492792/Tank%20Trouble%20Booster.user.js
-// @updateURL https://update.greasyfork.org/scripts/492792/Tank%20Trouble%20Booster.meta.js
+// @downloadURL https://update.greasyfork.org/scripts/492792/TankTrouble%20Booster.user.js
+// @updateURL https://update.greasyfork.org/scripts/492792/TankTrouble%20Booster.meta.js
 // ==/UserScript==
 
-// Disable unnecessary features
-function disableFeatures() {
-// Disable Chrome PDF Viewer
-chrome.plugins.mv = null;
+//Disable camera shake, lower the amount of feathers spawning
+UIConstants.MAX_CAMERA_SHAKE = 0;
+UIConstants.TANK_FEATHER_COUNT = 5
+UIConstants.TANK_FEATHER_POOL_SIZE = 10
 
-// Disable automatic tab discarding
-chrome.experimental.tabDiscarding = false;
-
-// Suspend unused tabs to free up resources
-chrome.tabs.query({}, function(tabs) {
-tabs.forEach(function(tab) {
-if (tab.url.indexOf("tanktrouble.com") === -1) {
-chrome.tabs.discard(tab.id);
-}
-});
-});
-
-// Reduce JavaScript timer frequency for background tabs
-chrome.extension.getBackgroundPage().setTimeout(function() {
-setInterval(function() {
-if (document.hidden) {
-// Adjust the delay interval to your preference (in milliseconds)
-// Higher values reduce CPU usage but may impact real-time updates
-setTimeout(function() {}, 300);
-}
-}, 1000);
-}, 5000);
-
-// Additional optimizations
-// Disable image animations
-const disableImageAnimations = () => {
-const images = document.querySelectorAll("img");
-images.forEach((img) => {
-img.style.animation = "none";
-});
-};
-    // Disable CSS animations
-const disableCSSAnimations = () => {
-const stylesheets = document.styleSheets;
-stylesheets.forEach((sheet) => {
-try {
-const rules = sheet.cssRules;
-rules.forEach((rule) => {
-if (rule.type === CSSRule.KEYFRAMES_RULE) {
-sheet.deleteRule(rule.name);
-}
-});
-} catch (error) {
-console.log("Error accessing stylesheet: " + error);
-}
-});
+//Lower the amount of particles spawning
+QualityManager.QUALITY_VALUES.auto = {
+    "tank explosion smoke count": 2,
+    "tank explosion fragment count": 2,
+    "missile launch smoke count": 0,
+    "missile smoke frequency": 360,
+    "mine explosion smoke count": 2,
+    "crate land dust count": 4,
+    "aimer min segment length":2,
+    "aimer off max segment length": 5.0,
+    "aimer on max segment length": 3.0,
+    "bullet puff count": 2,
+    "shield inverse bolt probability": 1,
+    "shield spark particles per emit": 1,
+    "spawn zone inverse unstable particle probability": 1,
+    "spawn zone num collapse particles": 3
 };
 
-disableImageAnimations();
-disableCSSAnimations();
-}
+QualityManager.QUALITY_VALUES.high = {
+    "tank explosion smoke count": 2,
+    "tank explosion fragment count": 2,
+    "missile launch smoke count": 0,
+    "missile smoke frequency": 360,
+    "mine explosion smoke count": 2,
+    "crate land dust count": 0,
+    "aimer min segment length": 2,
+    "aimer off max segment length": 5.0,
+    "aimer on max segment length": 3.0,
+    "bullet puff count": 2,
+    "shield inverse bolt probability": 1,
+    "shield spark particles per emit": 1,
+    "spawn zone inverse unstable particle probability": 1,
+    "spawn zone num collapse particles": 2
+};
 
-// Call the function to disable features
-disableFeatures();
+QualityManager.QUALITY_VALUES.low = {
+    "tank explosion smoke count": 1,
+    "tank explosion fragment count": 1,
+    "missile launch smoke count": 0,
+    "missile smoke frequency": 360,
+    "mine explosion smoke count": 1,
+    "crate land dust count": 0,
+    "aimer min segment length": 0,
+    "aimer off max segment length": 4.0,
+    "aimer on max segment length": 2.0,
+    "bullet puff count": 1,
+    "shield inverse bolt probability": 1,
+    "shield spark particles per emit": 0,
+    "spawn zone inverse unstable particle probability": 1,
+    "spawn zone num collapse particles": 1
+};
